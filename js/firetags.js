fdebd1f4378c6e7e411ecab8d28776b54d9f18d8
Lukáš Čech <ja@lukascech.cz>
2014-03-13 12:34:49 +0100

    hu

--- js/firetags.js
+++ js/firetags.js
@@ -1,4 +1,4 @@
 function fireTags(dest) {
-    dataLayer({event: 'virtual pageview', 'virtual url': '/destination/' + dest + '.html'});
+    dataLayer.push({event: 'virtual pageview', 'virtual url': '/destination/' + dest + '.html'});
 	console.log('fire tags');
 }
