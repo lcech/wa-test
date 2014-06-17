f5301723036c4a82aaaca548903af6f89bcc558d
Lukáš Čech <ja@lukascech.cz>
2014-06-17 14:41:57 +0200

    Update firetags.js

--- js/firetags.js
+++ js/firetags.js
@@ -1,15 +1,15 @@
 function fireTags(dest) {
-dataLayer.push ({ 
- 'ecommerce': { 
-  'detail': { 
-   'products': [{
-     'name': dest,
-     'id': '001', 
-     'category': 'Destinations/Europe'
-    }]
-     }
-   }
-});
-    dataLayer.push({event: 'virtual pageview', 'virtual url': '/destination/' + dest + '.html'});
-	console.log('fire tags');
+  dataLayer.push ({ 
+    'ecommerce': { 
+      'detail': { 
+        'products': [{
+          'id': dest, 
+          'name': dest,
+          'category': 'Destinations/Europe'
+         }]
+        }
+      }
+  });
+  dataLayer.push({event: 'virtual pageview', 'virtual url': '/destination/' + dest + '.html'});
+  console.log('fire tags');
 }
