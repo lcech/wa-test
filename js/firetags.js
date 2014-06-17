3aad2c0b7447e95a7f880b17917ac272ed560a88
Heulwen <hana.kalivodova@gmail.com>
2014-06-17 14:26:21 +0200

    Update firetags.js
    ec

--- js/firetags.js
+++ js/firetags.js
@@ -1,4 +1,15 @@
 function fireTags(dest) {
+dataLayer.push ({ 
+ 'ecommerce': { 
+  'detail': { 
+   'products': [{
+     'name': 'Schwitzerland',
+     'id': '001', 
+     'category': 'Destinations/Europe'
+    }]
+     }
+   }
+});
     dataLayer.push({event: 'virtual pageview', 'virtual url': '/destination/' + dest + '.html'});
 	console.log('fire tags');
 }
